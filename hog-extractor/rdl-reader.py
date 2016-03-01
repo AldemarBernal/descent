2f37ad3ef13b69bcd0467cdd1d4e8f02b7c5f176
aldemar.bernal <aldemar.bernal@gmail.com>
2016-03-01 12:41:16 -0500

    Read level vertices and walls

--- hog-extractor/rdl-reader.py
+++ hog-extractor/rdl-reader.py
@@ -1,7 +1,27 @@
+import json
 import os
 import struct
 import sys
 from collections import OrderedDict
+
+
+#RDL constants
+RDL_LEFT_SIDE = 0x01
+RDL_TOP_SIDE = 0x02
+RDL_RIGHT_SIDE = 0x04
+RDL_BOTTOM_SIDE = 0x08
+RDL_BACK_SIDE = 0x10
+RDL_FRONT_SIDE = 0x20
+RDL_ENERGY_CENTER = 0x40
+
+RDL_SIDES = [RDL_LEFT_SIDE, RDL_TOP_SIDE, RDL_RIGHT_SIDE, RDL_BOTTOM_SIDE, RDL_BACK_SIDE, RDL_FRONT_SIDE]
+RDL_SIDES_TEXT = {
+    RDL_LEFT_SIDE: 'left_side',
+    RDL_TOP_SIDE: 'top_side',
+    RDL_RIGHT_SIDE: 'right_side',
+    RDL_BOTTOM_SIDE: 'bottom_side',
+    RDL_BACK_SIDE: 'back_side',
+    RDL_FRONT_SIDE: 'front_side'}
 
 
 def show_usage():
@@ -36,25 +56,125 @@ def rdl_read(filename):
 def get_mine_data(rdl, offset):
     rdl.seek(offset)
 
-    read_byte(rdl)
-
+    version = read_ubyte(rdl)
     vertexCount = read_short(rdl)
     cubeCount = read_short(rdl)
 
-    vertices = read_vectors(rdl, vertexCount)
-
+    print('compiled version: ' + str(version))
     print('vertex count: ' + str(vertexCount))
     print('cube count: ' + str(cubeCount))
 
-    print('vertices:')
-    print(vertices)
+    vertices = read_vectors(rdl, vertexCount)
+
+#    for ptr, vertex in vertices.items():
+#        print(str(ptr) + ':\t' + str(vertex['x']) + '\t' + str(vertex['y'])  + '\t' + str(vertex['z']))
+
+
+    cubes = []
+    for cubeIndex in range(0, cubeCount):
+        print rdl.tell()
+
+        cube = OrderedDict()
+
+        bitmask = read_byte(rdl)
+        if bitmask:
+            cube['attached_cubes'] = OrderedDict()
+
+            if bitmask & RDL_LEFT_SIDE:
+                cube['attached_cubes']['left_side'] = read_short(rdl)
+
+            if bitmask & RDL_TOP_SIDE:
+                cube['attached_cubes']['top_side'] = read_short(rdl)
+
+            if bitmask & RDL_RIGHT_SIDE:
+                cube['attached_cubes']['right_side'] = read_short(rdl)
+
+            if bitmask & RDL_BOTTOM_SIDE:
+                cube['attached_cubes']['bottom_side'] = read_short(rdl)
+
+            if bitmask & RDL_BACK_SIDE:
+                cube['attached_cubes']['back_side'] = read_short(rdl)
+
+            if bitmask & RDL_FRONT_SIDE:
+                cube['attached_cubes']['front_side'] = read_short(rdl)
+
+
+        cube['vertices'] = []
+        for index in range(0, 8):
+            cube['vertices'].append(read_short(rdl))
+
+
+        if bitmask & RDL_ENERGY_CENTER:
+            cube['energy_center'] = OrderedDict()
+            cube['energy_center']['special'] = read_ubyte(rdl)
+            cube['energy_center']['number'] = read_byte(rdl)
+            cube['energy_center']['value'] = read_short(rdl)
+
+
+        cube['static_light'] = float(read_short(rdl)) / 4096.0
+
+        wall_bitmask = read_byte(rdl)
+        if wall_bitmask:
+            cube['walls'] = OrderedDict()
+
+            if wall_bitmask & RDL_LEFT_SIDE:
+                cube['walls']['left_side'] = read_ubyte(rdl)
+
+            if wall_bitmask & RDL_TOP_SIDE:
+                cube['walls']['top_side'] = read_ubyte(rdl)
+
+            if wall_bitmask & RDL_RIGHT_SIDE:
+                cube['walls']['right_side'] = read_ubyte(rdl)
+
+            if wall_bitmask & RDL_BOTTOM_SIDE:
+                cube['walls']['bottom_side'] = read_ubyte(rdl)
+
+            if wall_bitmask & RDL_BACK_SIDE:
+                cube['walls']['back_side'] = read_ubyte(rdl)
+
+            if wall_bitmask & RDL_FRONT_SIDE:
+                cube['walls']['front_side'] = read_ubyte(rdl)
+
+
+        cube['textures'] = OrderedDict()
+
+        for side in RDL_SIDES:
+            if not (bitmask & side) or (wall_bitmask & side):
+                texture = read_ushort(rdl)
+                cube['textures'][RDL_SIDES_TEXT[side]] = OrderedDict()
+                cube['textures'][RDL_SIDES_TEXT[side]]['primary'] = texture & 0x7FFF
+
+                if texture & 0x8000:
+                    cube['textures'][RDL_SIDES_TEXT[side]]['secondary'] = read_short(rdl)
+
+                cube['textures'][RDL_SIDES_TEXT[side]]['vertices'] = []
+                for vertexIndex in range(0, 4):
+                    uvl = OrderedDict()
+
+                    uvl['u'] = float(read_short(rdl)) / 4096.0
+                    uvl['v'] = float(read_short(rdl)) / 4096.0
+                    uvl['l'] = float(read_ushort(rdl)) / 4096.0
+
+                    cube['textures'][RDL_SIDES_TEXT[side]]['vertices'].append(uvl)
+
+
+        cubes.append(cube)
+
+        print(json.dumps(cube, indent = 4))
+
 
 
 
 def read_byte(file):
+    return struct.unpack('<b', file.read(1))[0]
+
+def read_ubyte(file):
     return struct.unpack('<B', file.read(1))[0]
 
 def read_short(file):
+    return struct.unpack('<h', file.read(2))[0]
+
+def read_ushort(file):
     return struct.unpack('<H', file.read(2))[0]
 
 def read_integer(file):
@@ -62,9 +182,9 @@ def read_integer(file):
 
 def read_vector(file):
     vector = OrderedDict()
-    vector['x'] = read_integer(file)
-    vector['y'] = read_integer(file)
-    vector['z'] = read_integer(file)
+    vector['x'] = float(read_integer(file)) / 65536.0
+    vector['y'] = float(read_integer(file)) / 65536.0
+    vector['z'] = float(read_integer(file)) / 65536.0
     return vector
 
 def read_vectors(file, max_submodels):
